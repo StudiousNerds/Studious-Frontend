917bdf434d6b680b76c87916a32fcdf629296851
이민희 <mhl98@dgu.ac.kr>
2023-09-11 16:50:15 +0900

    feat: 로그인 리다이렉트 체크 토큰 반환 값 추가

--- src/hooks/useRedirectLogin.js
+++ src/hooks/useRedirectLogin.js
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 const useRedirectLogin = (isDirectAccessWithUrl = false) => {
   const navigate = useNavigate();
+  const token = getCookie("accessToken");
   const handleRedirect = () => {
     try {
-      if (!getCookie("accessToken")) {
+      if (!token) {
         if (
           window.confirm(
             "로그인이 필요합니다.\n로그인 페이지로 이동하시겠습니까?"
@@ -25,6 +26,7 @@ const useRedirectLogin = (isDirectAccessWithUrl = false) => {
   };
 
   return {
+    token,
     handleRedirect,
   };
 };
