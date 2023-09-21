dbb63a4bb51cac8ef045408c77590c1c6525670a
이민희 <mhl98@dgu.ac.kr>
2023-09-22 01:23:28 +0900

    fix: env 스트링 품

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "process.env.REACT_APP_BASE_URL;";
+const baseURL = process.env.REACT_APP_BASE_URL;
 const request = async ({ url, method, body, params, token }) => {
   try {
     const config = {
