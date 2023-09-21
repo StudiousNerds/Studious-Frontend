8ff313846e8b16fbe0d678d603f83a738761c65f
hanjuuuuuu <hanjucomss@naver.com>
2023-09-22 04:41:48 +0900

    fix: search 문제 해결

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -7,6 +7,7 @@ import StudyCafeGridItem from "components/StudyCafeGridItem";
 import Pagination from "components/Pagination";
 import { GET } from "apis/api";
 import useSearchResult from "hooks/queries/useSearchResult";
+import StudyCafeGridSearch from "components/StudyCafeGridSearch";
 
 const SearchResult = () => {
   const location = useLocation();
@@ -126,8 +127,8 @@ const SearchResult = () => {
       )}
 
       <GridContainer>
-        {displayedItems.map((item, index) => (
-          <StudyCafeGridItem key={item.Id} item={item} index={index} />
+        {displayedItems.map((item) => (
+          <StudyCafeGridSearch key={item.Id} item={item} />
         ))}
       </GridContainer>
 
