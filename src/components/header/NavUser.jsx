89d52fb6c3459018f72c593be2d05f5a39a48b9f
이민희 <mhl98@dgu.ac.kr>
2023-10-06 18:38:40 +0900

    feat: 리스트 드롭다운 추가, 헤더 메뉴 추가

--- src/components/header/NavUser.jsx
+++ src/components/header/NavUser.jsx
@@ -4,9 +4,31 @@ import { ReactComponent as MessageIcon } from "assets/icons/message.svg";
 import { ReactComponent as DefaultProfileIcon } from "assets/icons/defaultProfile.svg";
 import { ReactComponent as MenuIcon } from "assets/icons/menu.svg";
 import { useLogoutMutation } from "hooks/queries/useLogout";
+import { useNavigate } from "react-router-dom";
+import ListDropdown from "components/common/ListDropdown";
 
 const NavUser = () => {
-  const handleLogoutMutation = useLogoutMutation();
+  const navigate = useNavigate();
+  const logoutMutation = useLogoutMutation();
+  const dropdownListItemsObjArr = [
+    {
+      itemName: "예약 관리",
+      itemClickEventHandler: () => navigate(`/myPage/reservation`),
+    },
+    {
+      itemName: "리뷰",
+      itemClickEventHandler: () => navigate(`/myPage/reviews`),
+    },
+    {
+      itemName: "북마크",
+      itemClickEventHandler: () => {},
+    },
+    {
+      itemName: "로그아웃",
+      itemClickEventHandler: () => logoutMutation.mutate(),
+      hasBorderTop: true,
+    },
+  ];
   return (
     <NavLayout>
       <NavButton>
@@ -18,10 +40,12 @@ const NavUser = () => {
       <NavButton>
         <DefaultProfileIcon />
       </NavButton>
-      <NavButton>
-        <MenuIcon />
-        <div onClick={() => handleLogoutMutation.mutate()}>로그아웃</div>
-      </NavButton>
+      <NavItem>
+        <ListDropdown
+          buttonChildren={<MenuIcon />}
+          listItemsObjArr={dropdownListItemsObjArr}
+        />
+      </NavItem>
     </NavLayout>
   );
 };
@@ -29,9 +53,13 @@ const NavUser = () => {
 export default NavUser;
 const NavLayout = styled.div`
   display: flex;
+  align-items: center;
   gap: 1rem;
   z-index: 5;
 `;
 const NavButton = styled.button`
+  width: 5rem;
+`;
+const NavItem = styled.div`
   width: 5rem;
 `;
