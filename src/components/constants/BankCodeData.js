a50dde15de79fac8be249cb93f40bdd71f53505e
이민희 <mhl98@dgu.ac.kr>
2023-10-08 16:57:13 +0900

    feat: 은행 코드 추가

---
+++ src/components/constants/BankCodeData.js
@@ -0,0 +1,102 @@
+export const BANK_CODES_DATA = [
+  {
+    bankName: "경남은행",
+    bankCode: "39",
+  },
+  {
+    bankName: "광주은행",
+    bankCode: "34",
+  },
+  {
+    bankName: "단위농협(지역농축협)",
+    bankCode: "12",
+  },
+  {
+    bankName: "부산은행",
+    bankCode: "32",
+  },
+  {
+    bankName: "새마을금고",
+    bankCode: "45",
+  },
+  {
+    bankName: "산림조합",
+    bankCode: "64",
+  },
+  {
+    bankName: "신한은행",
+    bankCode: "88",
+  },
+  {
+    bankName: "신협",
+    bankCode: "48",
+  },
+  {
+    bankName: "씨티은행",
+    bankCode: "27",
+  },
+  {
+    bankName: "우리은행",
+    bankCode: "20",
+  },
+  {
+    bankName: "우체국예금보험",
+    bankCode: "71",
+  },
+  {
+    bankName: "저축은행중앙회",
+    bankCode: "50",
+  },
+  {
+    bankName: "전북은행",
+    bankCode: "37",
+  },
+  {
+    bankName: "제주은행",
+    bankCode: "35",
+  },
+  {
+    bankName: "카카오뱅크",
+    bankCode: "90",
+  },
+  {
+    bankName: "토스뱅크",
+    bankCode: "92",
+  },
+  {
+    bankName: "하나은행",
+    bankCode: "81",
+  },
+  {
+    bankName: "홍콩상하이은행",
+    bankCode: "54",
+  },
+  {
+    bankName: "IBK기업은행",
+    bankCode: "03",
+  },
+  {
+    bankName: "KB국민은행",
+    bankCode: "06",
+  },
+  {
+    bankName: "DGB대구은행",
+    bankCode: "31",
+  },
+  {
+    bankName: "KDB산업은행",
+    bankCode: "02",
+  },
+  {
+    bankName: "NH농협은행",
+    bankCode: "11",
+  },
+  {
+    bankName: "SC제일은행",
+    bankCode: "23",
+  },
+  {
+    bankName: "Sh수협은행",
+    bankCode: "07",
+  },
+];
