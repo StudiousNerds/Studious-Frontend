f4d3e06dbc110009fb9ae49dcad632faf1461ff4
이민희 <mhl98@dgu.ac.kr>
2023-10-19 02:36:17 +0900

    fix: format number undefined 예외처리

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -3,5 +3,7 @@
  * @param {number} num 가공할 숫자 데이터
  * @returns 천 단위로 콤마(,)를 삽입한 문자열을 반환
  */
-export const formatNumberWithCommas = (num) =>
+export const formatNumberWithCommas = (num) => {
+  if (!num) return num;
   num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
